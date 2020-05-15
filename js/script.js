fa879506412e747c45e13c5428fca05bfd09df1a
Jamie Maxwell <jdsmax19@gmail.com>
2020-05-15 10:11:11 +0100

    skills and experience

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 // page scroll indicator
 
-window.onscroll = function() {
-  pageScroll()
-}
+// window.onscroll = function() {
+//   pageScroll()
+// }
 
-function pageScroll() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight
-  var scrolled = (winScroll / height) * 100
-  document.getElementById('myBar').style.width = scrolled + '%'
-}
+// function pageScroll() {
+//   var winScroll = document.body.scrollTop || document.documentElement.scrollTop
+//   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight
+//   var scrolled = (winScroll / height) * 100
+//   document.getElementById('myBar').style.width = scrolled + '%'
+// }
 
 // toggleNav
 
@@ -24,14 +26,7 @@ function toggleNav() {
 
 // about sidebar
 
-function openNav() {
-  document.getElementById('aboutSidepanel').style.width = '20%'
-}
 
-/* Set the width of the sidebar to 0 (hide it) */
-function closeNav() {
-  document.getElementById('aboutSidepanel').style.width = '0'
-}
 // Text rotation
 
 var TxtRotate = function(el, toRotate, period) {
@@ -92,3 +87,31 @@ window.onload = function() {
   css.innerHTML = '.txt-rotate > .wrap { border-right: 0.08em solid #666 }'
   document.body.appendChild(css)
 }
+
+// smooth scroll
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $('a').on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== '') {
+      // Prevent default anchor click behavior
+      event.preventDefault()
+
+      // Store hash
+      var hash = this.hash
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash
+      })
+    } // End if
+  })
+})
+
